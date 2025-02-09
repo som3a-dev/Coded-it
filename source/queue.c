0bef9d47d45095132da057f363e5867cd5b8a643
flex <omareltayeb529@gmail.com>
2025-02-09 05:13:06 +0200

    General purpose Queue

---
+++ source/queue.c
@@ -0,0 +1,98 @@
+#include <memory.h>
+#include <stdlib.h>
+#include "queue.h"
+
+void Queue_init(Queue* queue, size_t element_size)
+{
+    memset(queue, 0, sizeof(Queue));
+
+    queue->element_size = element_size;
+}
+
+
+void Queue_push(Queue* queue, char* element)
+{
+    if (!queue) return;
+    if (queue->element_size == 0) return;
+
+    if (queue->arr)
+    {
+        queue->len++;
+        queue->arr = realloc(queue->arr, queue->element_size * queue->len);
+        
+        char* element_start = queue->arr + (queue->len-1) * queue->element_size;
+        memcpy(element_start, element, queue->element_size);
+    }
+    else
+    {
+        queue->len = 1;
+        queue->arr = malloc(queue->element_size);
+        memcpy(queue->arr, element, queue->element_size);
+    }
+}
+
+
+char* Queue_pop(Queue* queue, bool return_element)
+{
+    if (!queue) return NULL;
+    if (queue->element_size == 0) return NULL;
+    if (!(queue->arr)) return NULL;
+    if (queue->len == 0) return NULL;
+
+    char* copy = NULL;
+    if (return_element)
+    {
+        char* element = queue->arr + (queue->len-1) * queue->element_size;
+        copy = malloc(queue->element_size);
+        memcpy(copy, element, queue->element_size);
+    }
+
+    queue->len--;
+    if (queue->len > 0)
+    {
+        queue->arr = realloc(queue->arr, queue->len * queue->element_size);
+    }
+    else
+    {
+        free(queue->arr);
+        queue->arr = NULL;
+    }
+
+    return copy;
+}
+
+
+void Queue_clear(Queue* queue)
+{
+    if (!queue) return;
+    
+    if (queue->arr)
+    {
+        free(queue->arr);
+        queue->arr = NULL;
+    }
+
+    queue->len = 0;
+}
+
+
+void Queue_print(Queue* queue)
+{
+    if (!queue) return;
+    for (int i = 0; i < queue->len; i++)
+    {
+        printf("%hhu", queue->arr[i]);
+        if (i != (queue->len-1))
+        {
+            printf(", ");
+        }
+    } 
+
+    printf("\n");
+}
+
+
+
+
+
+
---
+++ source/queue.h
@@ -0,0 +1,23 @@
+#include <stdbool.h>
+
+typedef struct
+{
+    char* arr;
+    int len;
+    int element_size;
+} Queue;
+
+
+
+void Queue_init(Queue* queue, size_t element_size);
+void Queue_push(Queue* queue, char* element);
+
+//NOTE(omar): THIS FUNCTION RETURNS A COPY OF THE LAST ELEMENT.
+//            MAKE SURE TO FREE() AFTER YOU ARE DONE
+char* Queue_pop(Queue* queue, bool return_element); 
+
+void Queue_clear(Queue* queue);
+
+
+//for testing
+void Queue_print(Queue* queue);
