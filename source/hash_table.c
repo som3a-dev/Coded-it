95d2115a4799c8d555a2123f05313b95ec7ee446
flex <omareltayeb529@gmail.com>
2025-05-14 08:24:44 +0300

    general purpose hash table
    hash table with variable element/value size. and strings as keys. as well as index collision response.

---
+++ source/hash_table.c
@@ -0,0 +1,146 @@
+#include <assert.h>
+#include <malloc.h>
+#include <memory.h>
+#include <stdlib.h>
+
+#include "hash_table.h"
+
+#define FNV_OFFSET 14695981039346656037UL
+#define FNV_PRIME 1099511628211UL
+
+#define TABLE_EXPAND_COUNT 5 //the amount of elements the table is expanded by
+                             //when a resize is needed (in case of an index collision)
+
+static uint64_t fnv_hash(const char* key);
+
+
+void hash_table_init(hash_table* table, int initial_len,
+                     size_t element_size)
+{
+    assert(initial_len >= 0 && "Length of container less than 0 ?");
+    assert(element_size >= 0 && "Size of element less than 0 ?");
+
+    table->len = initial_len;
+    table->element_size = element_size;
+
+    table->vals = calloc(table->len, table->element_size);
+    table->key_hashes = calloc(table->len, sizeof(uint64_t));
+}
+
+
+void hash_table_set(hash_table* table, const char* key, const char* val)
+{
+    uint64_t hash = fnv_hash(key);
+    uint64_t index = hash % table->len;
+
+//    printf("Key: %s, Index: %llu, Hash: %llu\n", key, index, hash);
+
+    if (table->key_hashes[index])
+    {
+        if (table->key_hashes[index] != hash)
+        {
+            _hash_table_resize(table, table->len + TABLE_EXPAND_COUNT);
+            hash_table_set(table, key, val);
+            return;
+        }
+    }
+    
+    for (int i = 0; i < table->element_size; i++)
+    {
+        table->vals[index + i] = val[i];
+    }
+
+    table->key_hashes[index] = hash;
+
+/*    for (int i = 0; i < table->len; i++)
+    {
+        printf("%llu, ", table->key_hashes[i]);
+    }
+    printf("\n");*/
+}
+
+
+char* hash_table_get(const hash_table* table, const char* key)
+{
+    uint64_t index = fnv_hash(key) % table->len;
+    
+    return table->vals + index;
+}
+
+
+void _hash_table_resize(hash_table* table, int new_len)
+{
+    // table->vals = realloc(table->vals, new_len * table->element_size);
+
+    // table->key_hashes = realloc(table->key_hashes, new_len * sizeof(uint64_t));
+
+    // memset(table->key_hashes + table->len, 0,
+    //        sizeof(uint64_t) * (new_len - table->len));
+    
+    // memset(table->vals + table->len, 0,
+    //        table->element_size * (new_len - table->len));
+        // 
+    // table->len = new_len;
+
+    //create new key hashes and values arrays of the new_len. and recopy the
+    //key value pairs while updating their indices to be the hash % the new length of
+    //the table
+
+    char* new_vals = calloc(new_len, sizeof(char));
+    uint64_t* new_key_hashes = calloc(new_len, sizeof(uint64_t));
+
+    uint64_t hash;
+    char* val;
+    int new_index = -1;
+    for (int i = 0; i < table->len; i++)
+    {
+        //get the key, value pair at index i
+        hash = table->key_hashes[i];
+        val = table->vals + (i * table->element_size);
+
+        //generate the new index (since the hash table's length changed)
+        new_index = hash % new_len; 
+
+        //set the new index of the hash to the hash in the new key hashes array
+        new_key_hashes[new_index] = hash;
+        
+        //same for the values array
+        val = table->vals + (i * table->element_size);
+        for (int j = 0; j < table->element_size; j++)
+        {
+            new_vals[new_index + j] = *(val + j);
+        }
+    }
+
+    free(table->vals);
+    free(table->key_hashes);
+
+    table->vals = new_vals;
+    table->key_hashes = new_key_hashes;
+
+    table->len = new_len;
+}
+
+
+void hash_table_print(const hash_table* table)
+{
+    for (int i = 0; i < table->len; i++)
+    {
+        printf("%d: %llu\n", i, table->key_hashes[i]);
+    }
+}
+
+
+//FNV-1a
+static uint64_t fnv_hash(const char* key)
+{
+    uint64_t hash = FNV_OFFSET;
+
+    for (int i = 0; i < strlen(key); i++)
+    {
+        hash ^= key[i];
+        hash *= FNV_PRIME;
+    }
+
+    return hash;
+}
---
+++ source/hash_table.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <stdint.h>
+
+
+typedef struct
+{
+    uint64_t* key_hashes; //the fnv hashes of each key.
+                     //NOT the index (which is hash % table->len)
+
+    char* vals; //the values
+    int len;
+    size_t element_size; //NEVER CHANGE on your own
+} hash_table;
+
+
+
+//MUST BE CALLED
+void hash_table_init(hash_table* table, int initial_len,
+                     size_t element_size);
+
+void hash_table_set(hash_table* table, const char* key,
+                    const char* val);
+
+//returns a reference to the element in the hash table's
+//internal valsay. not a copy of the element
+char* hash_table_get(const hash_table* table, const char* key);
+
+void hash_table_print(const hash_table* table);
+
+void _hash_table_resize(hash_table* table, int new_len);
+
