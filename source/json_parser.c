e124851aa417c42d84fb5f69dc18100129938e9c
flex <omareltayeb529@gmail.com>
2025-06-07 06:12:44 +0300

    Basic json lexer
    massive rewrite of the json parser. now lexing and parsing are actually seperated.

--- source/json_parser.c
+++ source/json_parser.c
@@ -1,29 +1,68 @@
+#include <malloc.h>
+#include <memory.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "json_parser.h"
 #include "string.h"
 #include "hash_table.h"
 
 
-
-
 enum
 {
-    JP_TOKEN_NONE,
-    JP_TOKEN_KEY,
-    JP_TOKEN_VALUE
+    JSON_TOKEN_NONE,
+    JSON_TOKEN_STRING,
+    JSON_TOKEN_INT,
+    JSON_TOKEN_BOOL,
+    JSON_TOKEN_CHAR
 };
+
+typedef struct
+{
+    int type;
+
+    //this is a real allocated pointer only in the case of strings
+    //in other cases (int, char) this is an encoded pointer
+    //meaning the pointer variable directly stores the value of the token
+    //no memory allocation needed
+    char* val;
+} json_token;
+
+
+static inline void json_token_set_int(json_token* token, int val);
+static inline void json_token_set_string(json_token* token, char* val);
+static inline void json_token_set_bool(json_token* token, bool val);
+static inline void json_token_set_char(json_token* token, char val);
+
+void json_token_print(json_token* token);
+
+void json_token_destroy(json_token* token);
+
+
+//helper for jp_lex()
+static void _resize_tokens_array(json_token** tokens, int* tokens_count, int new_count);
 
 
 bool is_index_part_of_literal(const char* text, int index);
+
+void jp_lex(const char* str);
+
+String jp_lex_string(const char* str);
+
+String jp_lex_numeric(const char* str);
+
+//str isn't changed after the function is done. it is mutated in the function
+//and then reset back to its original state
+//so it is practically const
+String jp_lex_bool(char* str);
 
 
 void jp_parse_file(const char* path)
 {
     FILE* fp;
-    fopen_s(&fp, path, "r");
+    fopen_s(&fp, path, "rb");
 
     if (!fp)
     {
@@ -31,107 +70,289 @@ void jp_parse_file(const char* path)
         return;
     }
 
-    hash_table table;
-    hash_table_init(&table, 0, sizeof(String));
+    char* file_string = NULL;
+    long file_len = 0;
 
-    char c;
-    String token = {0};
-    int token_type = JP_TOKEN_NONE;
+    fseek(fp, 0L, SEEK_END);
+    file_len = ftell(fp);
+    fseek(fp, 0L, SEEK_SET);
 
-    String current_key = {0};
-    while (!feof(fp))
-    {
-        c = fgetc(fp);
+    file_string = malloc(sizeof(char) * (file_len+1));
+    fread_s(file_string, sizeof(char) * file_len, sizeof(char), file_len, fp);
+    file_string[file_len] = '\0';
 
-        switch (c)
-        {
-            case '"':
-            {
-                if (token_type == JP_TOKEN_NONE)
-                {
-                    token_type = JP_TOKEN_KEY;
-                    continue;
-                }
-                else if (token_type == JP_TOKEN_KEY)
-                {
-                    token_type = JP_TOKEN_NONE;
-                    printf("Key: %s\n", token.text);
+    printf("%s\n", file_string);
 
-                    String_set(&current_key, token.text);
-                    String_clear(&token);
-                }
-            } break;
+    //lexing
+    jp_lex(file_string);
 
-            case ':':
-            {
-                if (token_type == JP_TOKEN_NONE)
-                {
-                    token_type = JP_TOKEN_VALUE;
-                    continue;
-                }
-            } break;
-
-            case '{':
-            {
-                if (token_type == JP_TOKEN_VALUE)
-                {
-                    if (is_index_part_of_literal(token.text, token.len) == false)
-                    {
-                        //the '{' is not part of a string literal. treat it accordingly
-                //        token_type = JP_TOKEN_NONE;
-                    }
-                }
-            } break;
-
-            case '\n':
-            {
-                if (token_type == JP_TOKEN_VALUE)
-                {
-                    token_type = JP_TOKEN_NONE;
-
-                    if (token.text[token.len-1] == ',')
-                    {
-                        String_pop(&token);
-                    }
-                    printf("Value: %s\n", token.text);
-
-                    if (current_key.text)
-                    {
-                        hash_table_set(&table, current_key.text, &token);
-                    }
-
-                    String_clear_without_freeing(&token);
-                }
-            } break;
-        }
-
-        if (token_type && (c != ' '))
-        {
-            String_push(&token, c);
-        }
-    }
-
-
-    hash_table_print(&table);
-    printf("\n");
-    for (int i = 0; i < table.len; i++)
-    {
-        String* str = hash_table_get_by_index(&table, i);
-        if (str)
-        {
-            if (str->text)
-            {
-                printf("%d: %s\n", i, str->text);
-                String_clear(str);
-            }
-        }
-    }
-
-    String_clear(&current_key);
-    String_clear(&token);
-
-    hash_table_clear(&table);
     fclose(fp);
+    free(file_string);
+}
+
+
+void jp_lex(const char* str)
+{
+    json_token* tokens = NULL;
+    int tokens_count = 0;
+
+    while (str[0])
+    {
+        String json_str = jp_lex_string(str);
+        if (json_str.text)
+        {
+            str += json_str.len;
+
+            _resize_tokens_array(&tokens, &tokens_count, tokens_count+1);
+
+            //DON'T CLEAR THE json_str.
+            //json_token_set_string doesn't copy the string. it just uses the existing one in json_str
+            //the string should be freed later
+            json_token_set_string(tokens + (tokens_count-1), json_str.text);
+
+//            printf("%s\n", tokens[tokens_count-1].val);
+            continue;
+        }
+
+        String json_numeric = jp_lex_numeric(str);
+        if (json_numeric.text)
+        {
+            str += json_numeric.len;
+
+            _resize_tokens_array(&tokens, &tokens_count, tokens_count+1);
+
+            int json_numeric_value = atoi(json_numeric.text);
+            json_token_set_int(tokens + (tokens_count-1), json_numeric_value);
+
+            String_clear(&json_numeric);
+            continue;
+        }
+
+        String json_bool = jp_lex_bool(str);
+        if (json_bool.text)
+        {
+//            printf("%s\n", json_bool.text);
+            str += json_bool.len;
+
+            _resize_tokens_array(&tokens, &tokens_count, tokens_count+1);
+
+            bool json_bool_value = false;
+            if (strcmp(json_bool.text, "true") == 0)
+            {
+                json_bool_value = true;
+            }
+            json_token_set_bool(tokens + (tokens_count-1), json_bool_value);
+
+            String_clear(&json_bool);
+            continue;
+        }
+
+        switch (str[0])
+        {
+            case ':':
+            case '[':
+            case ']':
+            case '{':
+            case '}':
+            case ',':
+            {
+//                printf("%c\n", str[0]);
+                _resize_tokens_array(&tokens, &tokens_count, tokens_count+1);
+                json_token_set_char(tokens + (tokens_count-1), str[0]);
+            } break;
+        }
+        
+        str++;
+    }
+
+    for (int i = 0; i < tokens_count; i++)
+    {
+        json_token_print(tokens + i);
+    }
+
+    for (int i = 0; i < tokens_count; i++)
+    {
+        json_token_destroy(tokens + i);
+    }
+}
+
+
+String jp_lex_string(const char* str)
+{
+    String result = {0};
+    int len = strlen(str);
+
+    if (str[0] == '"')
+    {
+        String_push(&result, '"');
+        for (int i = 1; i < len; i++)
+        {
+            if (str[i] == '"')
+            {
+                break;
+            }
+
+            String_push(&result, str[i]);
+        }
+        String_push(&result, '"');
+    }
+
+    return result;
+}
+
+
+String jp_lex_numeric(const char* str)
+{
+    String number_string = {0}; //the string containing the number we lexed
+    int len = strlen(str);
+
+    for (int i = 0; i < len; i++)
+    {
+        if (str[i] < '0') break;
+        if (str[i] > '9') break;
+
+        String_push(&number_string, str[i]);
+    }
+
+    return number_string;
+}
+
+
+String jp_lex_bool(char* str)
+{
+    String bool_string = {0};
+    int len =  strlen(str);
+
+    //check for true
+    if (len >= 4)
+    {
+        char temp = str[4];
+        str[4] = '\0';
+
+        if (strcmp(str, "true") == 0)
+        {
+            String_set(&bool_string, "true");
+            str[4] = temp;
+            return bool_string;
+        }
+
+        str[4] = temp;
+    }
+
+    //check for false
+    if (len >= 5)
+    {
+        char temp = str[5];
+        str[5] = '\0';
+
+        if (strcmp(str, "false") == 0)
+        {
+            String_set(&bool_string, "false");
+            str[5] = temp;
+            return bool_string;
+        }
+
+        str[5] = temp;
+    }
+
+    return bool_string;
+}
+
+
+static void _resize_tokens_array(json_token** tokens, int* tokens_count, int new_count)
+{
+    if (new_count < 0) return;
+    if (new_count == *tokens_count) return;
+
+    if ((*tokens))
+    {
+        (*tokens) = realloc(*tokens, new_count * sizeof(json_token));
+
+        if (new_count > (*tokens_count))
+        {
+            json_token* new_memory = (*tokens) + (*tokens_count);
+            memset(new_memory, 0, sizeof(json_token) * (new_count - (*tokens_count)));
+        }
+    }
+    else
+    {
+        (*tokens) = calloc(new_count, sizeof(json_token));
+    }
+
+    *tokens_count = new_count;
+}
+
+
+static inline void json_token_set_int(json_token* token, int val)
+{
+    token->type = JSON_TOKEN_INT;
+    token->val = val;
+}
+
+
+static inline void json_token_set_string(json_token* token, char* val)
+{
+    token->type = JSON_TOKEN_STRING;
+    token->val = val;
+}
+
+
+static inline void json_token_set_bool(json_token* token, bool val)
+{
+    token->type = JSON_TOKEN_BOOL;
+    token->val = val;
+}
+
+
+static inline void json_token_set_char(json_token* token, char val)
+{
+    token->type = JSON_TOKEN_CHAR;
+    token->val = (char *)(uintptr_t)(unsigned char)val;
+}
+
+
+void json_token_print(json_token* token)
+{
+    switch (token->type)
+    {
+        case JSON_TOKEN_STRING:
+        {
+            printf("%s\n", token->val);
+        } break;
+
+        case JSON_TOKEN_INT:
+        {
+            printf("%d\n", token->val);
+        } break;
+
+        case JSON_TOKEN_BOOL:
+        {
+            const char* str = "true";
+            if (token->val == 0)
+            {
+                str = "false";
+            }
+            printf("%s\n", str);
+        } break;
+
+        case JSON_TOKEN_CHAR:
+        {
+            printf("%c\n", token->val);
+        } break;
+    }
+}
+
+
+void json_token_destroy(json_token* token)
+{
+    if (token->type == JSON_TOKEN_STRING)
+    {
+        if (token->val)
+        {
+            free(token->val);
+            token->val = NULL;
+        }
+    }
 }
 
 
@@ -148,6 +369,7 @@ bool is_index_part_of_literal(const char* text, int index)
 
     return ((quote_count % 2) != 0);
 }
+
 
 
 
